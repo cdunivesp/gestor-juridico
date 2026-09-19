3c8404e602768c352fc4789a34a31b4c63ec0dbc
cdunivesp <24205547@aluno.univesp.br>
2026-09-19 00:56:48 -0300

    Adiciona layout completo com base, cadastro e consulta

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('consulta/', views.consulta, name='consulta'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Cliente, Processo
 
 def home(request):
     return render(request, 'core/index.html')
+
+def cadastro(request):
+    return render(request, 'core/cadastro.html')
+
+def consulta(request):
+    processos = Processo.objects.all()
+    return render(request, 'core/consulta.html', {'processos': processos})
